e0b06d37f503100dac63b71b4996f2f98b909f48
dev-tpghks23 <tpghksgit23@gmail.com>
2025-08-18 17:47:54 +0900

    day02

---
+++ variable/src/castingTest/CastingTest.java
@@ -0,0 +1,35 @@
+package castingTest;
+
+public class CastingTest {
+	public static void main(String[] args) {
+//		정수 + 실수 = 실수
+//		자동 형변환
+		System.out.println(1 + 2.5);
+		
+//		정수 + 정수 = 정수
+		System.out.println(5 + 3);
+		
+//		정수 / 정수 = 정수
+		System.out.println(5 / 2);
+		System.out.println(5.0 / 2);
+		
+//		강제 형변환
+		System.out.println(5 / (double)2);
+		System.out.println((int)(5 / (double)2));
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
