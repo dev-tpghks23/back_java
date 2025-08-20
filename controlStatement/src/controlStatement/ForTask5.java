9cd4dd42e357f209fbe4a4df92f944df9d18023b
dev-tpghks23 <tpghksgit23@gmail.com>
2025-08-20 17:49:07 +0900

    day04

---
+++ controlStatement/src/controlStatement/ForTask5.java
@@ -0,0 +1,55 @@
+package controlStatement;
+
+import java.util.Scanner;
+
+public class ForTask5 {
+	public static void main(String[] args) {
+//		1. "ACEG...Y" 출력!
+//		for(int i = 0; i < 13; i++) {
+//			System.out.print((char)('A' + i * 2));
+//		}
+		
+//		2. 1 ~ N까지의 합 출력!
+//		Scanner sc = new Scanner(System.in);
+//		String message = "N번까지 더할 숫자를 입력하세요.\n55";
+//		int input = 0, result = 0;
+//		
+//		System.out.println(message);
+//		input = sc.nextInt();
+//		
+//		가우스 공식
+//		for(int i = 0; i < input; i++) {
+//			result += i + 1;
+//		}
+//		System.out.println(input * (input + 1) / 2);
+//		System.out.println(result);
+		
+		
+//		3. 0123401234 출력!
+//		String result3 = "";
+//		for(int i = 0; i < 10; i++) {
+//			result3 += i % 5;
+//		}
+//		System.out.println(Integer.parseInt(result3));
+		
+		
+//		4. 입력예시) a1b22c357
+//		abbbbbbbbbbbbbbccccccccccccccccccccccccccccccccc
+//		사용자가 입력한 횟수가 얼마가 있을지 모르고, 
+//		입력한 횟수만큼 반복해주는 프로그램 만들기
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
